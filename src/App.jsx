d22231465aa4ef8afa5134f666ba49e151f17371
Your Username <magoxaire@gmail.com.com>
2025-03-25 11:38:45 -0500

    casi termino

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import { Header } from './assets/components/header/header'
+import { Main } from './assets/components/home/home'
+import { Footer } from './assets/components/footer/footer'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
